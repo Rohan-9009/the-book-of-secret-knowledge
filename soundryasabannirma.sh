70313b065dffe24a11034734e46e0611df423ec6
Rohan-9009 <rohanlatane09@gmail.com>
2025-08-12 13:33:10 +0530

    soundrya-saban-nirma

---
+++ soundryasabannirma.sh
@@ -0,0 +1,17 @@
+echo "soundrya saban nirma"
+
+echo "tum husn pari tum jane jaha"
+echo "tum sabse hasi tum sabse jawa"
+echo "soundrya saban nirma"
+
+echo "kya rang khila kya rup saja"
+echo "maine hi nahi duniya ne kaha"
+echo "soundrya saban nirma"
+
+echo "yu khili khili yu savar savar"
+echo "a janejigar tum chsali kahan"
+echo "soundrya saban nirma"
+
+echo "tum husn pari tum jane jaha"
+echo "tum sabse hasi tum sabse jawa"
+echo "soundrya saban nirma"
